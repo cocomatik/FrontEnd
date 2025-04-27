ea0c4477be2eaeb1830ca4a10c62ead207723752
schdule-x <205230460+schdulex2025@users.noreply.github.com>
2025-04-27 14:07:09 +0530

    //

--- js/cart.js
+++ js/cart.js
@@ -43,7 +43,7 @@ fetch('https://engine.cocomatik.com/api/orders/cart/', {
       
 
       <div id="itemDetailsBox">
-        <div id="itemNAme">Namooo: ${product.name}</div>
+        <div id="itemNAme">Name: ${product.name}</div>
         <div id="itemDetail">${product.description}</div>
         <div id="itemMRP">MRP: $${product.mrp.toFixed(2)}</div>
         <div id="price">price: $${product.price}</div>
