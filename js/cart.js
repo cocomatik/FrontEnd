70fb317b794df1e173db8d1373a68e666015bf6f
schdule-x <205230460+schdulex2025@users.noreply.github.com>
2025-04-27 13:22:01 +0530

    //

--- js/cart.js
+++ js/cart.js
@@ -41,6 +41,8 @@ fetch('https://engine.cocomatik.com/api/orders/cart/', {
         <img src="/data2/hair2.png" alt="${product.title}" width="150">
       </section>
 
+      
+
       <section id="itemDetailsBox">
         <div id="itemNAme">Name: ${product.name}</div>
         <div id="itemDetail">${product.description}</div>
